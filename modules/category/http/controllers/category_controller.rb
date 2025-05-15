79b1eb841adbad5ee0371da4be142790b12f5a39
Thien Ho Van <thienhv@nal.vn>
2025-05-16 06:53:32 +0700

    [RB-7] Add docs function to controller category

--- modules/category/http/controllers/category_controller.rb
+++ modules/category/http/controllers/category_controller.rb
@@ -11,10 +11,10 @@ module Category
           @category_service = category_service
         end
 
-        # The `index` action responds with a JSON object containing a message.
-        # The message includes the name of the controller and the action.
+        # The `index` action retrieves a paginated list of categories, optionally filtered by a search query,
+        # and renders the result as a JSON response.
         #
-        # @return [JSON]
+        # @return [JSON] A JSON array of transformed category data.
         def index
           categories = @category_service.list(
             page: params[:page],
@@ -27,12 +27,19 @@ module Category
           render_success(categories, transformed_data: transformed)
         end
 
+        # The `show` action retrieves a single category by its ID and renders it as a JSON response.
+        #
+        # @return [JSON] A JSON object of the transformed category data.
         def show
           category = @category_service.find(params[:id])
 
           render_success(Category::Transformers::DetailResource.transform(category))
         end
 
+        # The `store` action creates a new category with the provided parameters,
+        # saves it to the database, and renders the created category as a JSON response.
+        #
+        # @return [JSON] A JSON object of the transformed created category data.
         def store
           category = @category_service.create(create_or_update_params)
           category.save!
@@ -40,12 +47,19 @@ module Category
           render_success(Category::Transformers::DetailResource.transform(category))
         end
 
+        # The `update` action updates an existing category with the provided parameters
+        # and renders the updated category as a JSON response.
+        #
+        # @return [JSON] A JSON object of the transformed updated category data.
         def update
           category = @category_service.update(params[:id], create_or_update_params)
 
           render_success(Category::Transformers::DetailResource.transform(category))
         end
 
+        # The `destroy` action deletes a category by its ID and renders a success message as a JSON response.
+        #
+        # @return [JSON] A JSON object containing a success message.
         def destroy
           @category_service.delete(params[:id])
 
@@ -54,6 +68,9 @@ module Category
 
         private
 
+        # Permits and returns the parameters required for creating or updating a category.
+        #
+        # @return [ActionController::Parameters] The permitted parameters for a category.
         def create_or_update_params
           params.require(:category).permit(:name, :description)
         end
